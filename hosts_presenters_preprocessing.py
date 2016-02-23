aab81df58640bfe48d40696ed0470bf1c88a04bd
LiamFlahive <williamflahive2012@u.northwestern.edu>
2016-02-22 19:51:45 -0600

    hosts working
    hi

--- hosts_presenters_preprocessing.py
+++ hosts_presenters_preprocessing.py
@@ -3,7 +3,8 @@ import nltk
 import operator
 import sys
 import re
-import winners
+from winners import find_winners
+
 
 #amar shah
 host_keywords = ["host", "hosting", "hosts", "hosted"]
@@ -14,7 +15,7 @@ stopwords = nltk.corpus.stopwords.words('english') + punctuation_stopword
 humor_keywords = ["hilarious", "funny", "comedian", "best joke", "hysterical"]
 
 #winners = {'cecil b. demille award' : 'Jodie Foster', 'best motion picture - drama' : 'Argo', 'best performance by an actress in a motion picture - drama' : 'Jessica Chastain', 'best performance by an actor in a motion picture - drama' : 'Daniel Day-Lewis', 'best motion picture - comedy or musical' : 'Les Miserables', 'best performance by an actress in a motion picture - comedy or musical' : 'Jennifer Lawrence', 'best performance by an actor in a motion picture - comedy or musical' : 'Hugh Jackman', 'best animated feature film' : 'Brave', 'best foreign language film' : 'Amour', 'best performance by an actress in a supporting role in a motion picture' : 'Anne Hathaway', 'best performance by an actor in a supporting role in a motion picture' : 'Christoph Waltz', 'best director - motion picture' : 'Ben Affleck', 'best screenplay - motion picture' : 'Quentin Tarantino', 'best original score - motion picture' : 'Mychael Danna', 'best original song - motion picture' : 'Skyfall', 'best television series - drama' : 'Homeland', 'best performance by an actress in a television series - drama' : 'Claire Danes', 'best performance by an actor in a television series - drama' : 'Damian Lewis', 'best television series - comedy or musical' : 'Girls', 'best performance by an actress in a television series - comedy or musical':'Lena Dunham', 'best performance by an actor in a television series - comedy or musical':'Don Cheadle', 'best mini-series or motion picture made for television':'Game Change', 'best performance by an actress in a mini-series or motion picture made for television':'Julianne Moore', 'best performance by an actor in a mini-series or motion picture made for television':'Kevin Costner', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television': 'Maggie Smith', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television': 'Ed Harris'}
-winners = find_winners(year)
+
 
 def loadParsedTweets(filename):
     try:
@@ -121,6 +122,7 @@ def getHumor(year):
     return comedians
 
 def getPresenters(year):
+    winners = find_winners(year)
     presenters = dict()
     presentersTweets = lookthroughTweets(presenters_keywords, year)
     for w in winners:
